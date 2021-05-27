7159e1a57a20262c69431b55842b80279c0a20dd
ohyung1287 <ohyung1287@gmail.com>
2021-05-26 20:09:54 -0400

    gitignore added

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 const LibraFactory = artifacts.require("LibraFactory");
-const Libra_Owner_Wallet = "0xA4f3B729A4D6bea2c13d427390cfF7754273e16C"; // dev wallet
+const Libra_Owner_Wallet = "0xDEb73BDFDfA07a21acdC59a084e2F9dBc89C8D08"; // dev wallet
 module.exports = function (deployer) {
-  deployer.deploy(LibraFactory,Libra_Owner_Wallet);
+  // deployer.deploy(LibraFactory,Libra_Owner_Wallet);
+  
 };
